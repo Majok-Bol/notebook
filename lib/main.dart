b5d49305c8de0961154c6611052d0327dff0b223
Majok-Bol <bolmajok001@gmail.com>
2025-03-14 14:14:36 +0300

    added textfield and save button

--- lib/main.dart
+++ lib/main.dart
@@ -2,51 +2,44 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MyApp());
 }
-//class to create home page
+//class to create the home page
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Notebook',style: TextStyle(fontSize: 20,color: Colors.deepOrangeAccent),),centerTitle: true,backgroundColor: Colors.black87,),
+        body:Padding(padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(child: TextField(
+                controller: null,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Write your notes here ',
+
+                ),
+              )),
+              // SizedBox(height: 10,),
+              ElevatedButton(onPressed: null, child: Text('Save notes',style:TextStyle(color: Colors.redAccent),))
+
+                ],
+          ),
+        ),
+
+        bottomNavigationBar:BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.save),label: 'My notes'),
+          BottomNavigationBarItem(icon: Icon(Icons.add),label: 'Add notes'),
+          BottomNavigationBarItem(icon: Icon(Icons.edit),label: 'Edit'),
+          BottomNavigationBarItem(icon: Icon(Icons.delete),label: 'Delete'),
+
+        ]),
       ),
-      home: MyHomePage(),
-    );
-
-  }
-}
-//class to create the home page state
-class MyHomePage extends StatefulWidget{
-  @override
-  MyHomePageState createState()=>MyHomePageState();
-}
-//class to set the home page
-class MyHomePageState extends State<MyHomePage>{
-  //set the counter to track the page clicked
-  int currentPage=0;
-  @override
-  Widget build(BuildContext context){
-    //build the home page screen
-    return Scaffold(
-      //add title
-      appBar: AppBar(title: Text('My notebook',style: TextStyle(fontSize: 28,color: Colors.redAccent),),centerTitle: true,),
-
-    body: Center(
-      child:currentPage==0?Text('Add notes'):currentPage==1?Text('Edit notes'):Text('Delete notes'),
-    ),
-      //add bottom navigation icons
-      bottomNavigationBar: BottomNavigationBar(currentIndex: currentPage,
-    onTap: (index){
-        setState(() {
-          currentPage=index;
-        });
-    },
-    items: [
-      BottomNavigationBarItem(icon:Icon(Icons.add),label: 'Add notes'),
-    BottomNavigationBarItem(icon:Icon(Icons.edit),label: 'Edit notes'),
-    BottomNavigationBarItem(icon:Icon(Icons.delete),label: 'Delete notes'),
-    ],
-    ),
     );
   }
 }
